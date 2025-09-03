3acaff87c890a393964f10637dee9e15414c3e27
siddhisingh9 <siddhiwrite@gmail.com>
2025-09-03 18:56:37 +0530

    removed n added files w redis url to .ignore

--- testredis.py
+++
@@ -1,6 +0,0 @@
-import redis, os, json
-
-r = redis.from_url('redis://default:xV9mjJOgsXwWuEHpFp6hliiSsPbdVzNN@redis-13193.crce179.ap-south-1-1.ec2.redns.redis-cloud.com:13193', decode_responses=True)
-
-print("Total rows:", r.llen("demo:rows"))
-print("First row:", json.loads(r.lindex("demo:rows", 0)))
